4e376309c440c8a0f0b8de6bcb3caba3413b710d
stacype <stacy.peairs@live.com>
2018-02-01 14:52:26 -0800

    commiting all our hard work and sucess

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 'use strict';
-
 const question1 = prompt('What state were you born in?');
-console.log('User reply: ' + question1);
-alert('You began life in ' + question1 + '!');
-
+function ask1() {
+    console.log('User reply: ' + question1);
+    alert('You began life in ' + question1 + '!');
+}
+ask1();
 
 const question2 = prompt('What city do you live in?');
-console.log('User reply: ' + question2);
-alert('You began life in ' + question1 + ' and you love ' + question2 + '?');
+function ask2() {
+    console.log('User reply: ' + question2);
+    alert('You began life in ' + question1 + ' and you love ' + question2 + '?');
+}
+ask2();
 
 const question3 = prompt('How are you getting home?');
-console.log('User reply: ' + question3);
-alert('You began life in ' + question1 + ' and you love ' + question2 + ' ?' + ' You\'re ' +  question3 + '-ing' + ' home. ');
+function ask3() {
+    console.log('User reply: ' + question3);
+    alert('You began life in ' + question1 + ' and you love ' + question2 + ' ?' + ' You\'re ' +  question3 + '-ing' + ' home. ');
+}
+ask3();
 
 const question4 = prompt('What on earth did you eat for breakfast?');
+function ask4() {
 console.log('User reply: ' + question4);
 alert('You began life in ' + question1 + ' and you love ' + question2 + '?' + ' You\'re ' +  question3 + '-ing' + ' home. ' + ' Really, You ate ' + question4 + '?' +  'That stuff is gonna kill you!');
+}
+ask4();
 
-
+function ask5() {
 const qOne = prompt('How many lies will Trump tell during the State of The Union address tonight?');
 
 if (qOne <= 1 ) {
@@ -31,7 +41,10 @@ else if (qOne >= 1 && qOne < 10){
     alert('Who care\'s let\'s just impeach him!');
 }
 console.log('User chose: ' + qOne);
+}
+ask5();
 
+function ask6through11() {
 const qTwo = prompt('Will Trump disrespect another culture tonight?');
 
 if(qTwo.toLowerCase() === 'yes' || qTwo.toLowerCase() === 'y'){
@@ -43,6 +56,7 @@ if(qTwo.toLowerCase() === 'yes' || qTwo.toLowerCase() === 'y'){
 }
 console.log('User chose: ' + qTwo);
 
+
 const qThree = prompt('Will Trump slur his words tonight ? ');
 if(qThree.toLowerCase() === 'yes' || qThree.toLowerCase() === 'y') {
     alert('I hope his dentures don\'t fall out on Prime Time T.V!');
@@ -50,9 +64,11 @@ if(qThree.toLowerCase() === 'yes' || qThree.toLowerCase() === 'y') {
     alert('That\'s very nice of him.');
 }   else{
     alert ('Two years, 11 months and 20 days to go!');
-}
+
 
 console.log('User chose: ' + qThree);
+}
+ 
 
 const qFour = prompt('Will Trump sound like an intelligent, qualified Leader of The Free World tonight?');
 if(qFour.toLowerCase() === 'yes' || qFour.toLowerCase === 'y'){
@@ -115,7 +131,8 @@ for(let i = 0; i <= 5; i++){
         question = prompt('What is one of my wives names?');
     }}
 alert('Congratulations ' + qSix + ', You got BLANK out of 7 questions correct');
-
+}
+ask6through11();
 
 
 
